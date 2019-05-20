59a66cc9e2a8292a9b7d2361efcfd1e7e95db511
mahssaiz <mahssaiz@gmail.com>
2019-05-20 09:22:33 +0200

    commit para añadir el fichero Clase3.java

---
+++ Tutorial/src/poo/Clase3.java
@@ -0,0 +1,12 @@
+package poo;
+
+public class Clase3 {
+// LINEA DE COMENTARIO PARA PRUEBA DE GIT el fichero Clase3.java
+	
+	protected int mivar = 5;
+	protected int mivar2 = 7;
+	protected String mimetodo() {
+		return "el valor de mivar2 es:"  + mivar2;
+	}
+	
+}
