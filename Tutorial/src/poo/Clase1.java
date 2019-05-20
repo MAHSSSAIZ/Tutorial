8d6d4c4d603844d482fece1512c001f2931c59d0
mahssaiz <mahssaiz@gmail.com>
2019-05-20 09:51:24 +0200

    nuevo commit con la variable mivar3 en el fichero Clase1.java

--- Tutorial/src/poo/Clase1.java
+++ Tutorial/src/poo/Clase1.java
@@ -3,6 +3,7 @@ package poo;
 public class Clase1 {
 // LINEA DE COMENTARIO PARA PRUEBA DE GIT
 	protected int mivar = 5;
+	int mivar3 = 53;
 	protected int mivar2 = 7;
 	protected String mimetodo() {
 		return "el valor de mivar2 es:"  + mivar2;
